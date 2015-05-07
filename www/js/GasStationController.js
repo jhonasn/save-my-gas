a904bf002e601e9f94b490d7358a11b1a027afe0
Jhonas Nascimento <jhonasn@gmail.com>
2015-05-07 03:23:48 -0300

    Implemented requirejs on the project

--- www/js/GasStationController.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('save-my-gas')
-
-app.controller('GasStationCtrl', function($scope, crud) {
-  $scope.gasStations = crud.getAll('station')
-})
-
-app.controller('GasStationViewCtrl', function($scope, $state, $stateParams, crud) {
-  $scope.station = crud.get('station', $stateParams.id)
-})
