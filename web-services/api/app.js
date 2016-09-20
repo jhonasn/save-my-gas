cbaa4fe3f86b41ec28b68f0dc4a0c9f54f71e5af
Jhonas Nascimento <jhonasn@gmail.com>
2016-09-20 02:09:38 -0300

    implementado api rest, (funcionando)

--- web-services/api/app.js
+++ web-services/api/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
@@ -9,19 +8,12 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routes);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- web-services/api/routes/index.js
+++ web-services/api/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send([
+	  'valor 1',
+	  'valor 2',
+	  'valor 3'
+  ])
+});
+
+router.get('/teste', function(req, res, next) {
+  return 'retorno do teste'
 });
 
 module.exports = router;
