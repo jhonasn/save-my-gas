b6480a23cde0aa1b56370833d29ca132cd080e75
jhonasn <jhonasn@gmail.com>
2016-09-28 00:31:44 -0300

    removido modulos de servidor html

--- web-services/api/app.js
+++ web-services/api/app.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var path = require('path')
-var favicon = require('serve-favicon')
 var logger = require('morgan')
-var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
 var routes = require('./routes/api')
@@ -10,12 +8,9 @@ var routes = require('./routes/api')
 var app = express()
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', routes)
 
