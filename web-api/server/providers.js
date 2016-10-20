a4cc61ba3becb1eaf966b24458e3b7ab902cdaaf
jhonasn <jhonasn@gmail.com>
2016-10-20 01:58:52 -0200

    auth providers config in production

--- web-api/server/providers.js
+++ web-api/server/providers.js
@@ -1,53 +1,60 @@
+var serverPath = 'http://192.168.2.116:3000';
+
+var env = process.env.NODE_ENV;
+if(typeof env === 'string' && env.toLowerCase() === 'production') {
+	serverPath = 'https://rest-savemygas.getup.io';
+}
+
 module.exports = {
-	// "local": {
-	// 	"provider": "local",
-	// 	"module": "passport-local",
-	// 	"usernameField": "username",
-	// 	"passwordField": "password",
-	// 	"authPath": "/auth/local",
-	// 	"successRedirect": "/auth/account",
-	// 	"failureRedirect": "/login",
-	// 	"session": false
-	// },
+	"local": {
+		"provider": "local",
+		"module": "passport-local",
+		"usernameField": "username",
+		"passwordField": "password",
+		"authPath": "/auth/local",
+		"successRedirect": serverPath + "/auth/account",
+		"failureRedirect": "/login",
+		"session": false
+	},
 	"google-login": {
 		"provider": "google",
 		"module": "passport-google-oauth",
 		"strategy": "OAuth2Strategy",
 		"clientID": process.env.GOOGLE_AUTH_PROVIDER_ID,
 		"clientSecret": process.env.GOOGLE_AUTH_PROVIDER_SECRET,
-		"callbackURL": "http://localhost:3000/auth/google/callback",
+		"callbackURL": serverPath + "/auth/google/callback",
 		"authPath": "/auth/google",
 		"callbackPath": "/auth/google/callback",
-		"successRedirect": "/api/users/me",
+		"successRedirect": serverPath + "/api/users/me",
 		"failureRedirect": "/login",
 		"scope": ["profile", "email"],
 		"session": false,
 		"json": true
 	},
-	// "google-link": {
-	// 	"provider": "google",
-	// 	"module": "passport-google-oauth",
-	// 	"strategy": "OAuth2Strategy",
-	// 	"clientID": process.env.GOOGLE_AUTH_PROVIDER_ID,
-	// 	"clientSecret": process.env.GOOGLE_AUTH_PROVIDER_SECRET,
-	// 	"callbackURL": "/link/google/callback",
-	// 	"authPath": "/link/google",
-	// 	"callbackPath": "/link/google/callback",
-	// 	"successRedirect": "/auth/account",
-	// 	"failureRedirect": "/login",
-	// 	"scope": ["email", "profile"],
-	// 	"link": true,
-	// 	"session": false
-	// },
+	"google-link": {
+		"provider": "google",
+		"module": "passport-google-oauth",
+		"strategy": "OAuth2Strategy",
+		"clientID": process.env.GOOGLE_AUTH_PROVIDER_ID,
+		"clientSecret": process.env.GOOGLE_AUTH_PROVIDER_SECRET,
+		"callbackURL": serverPath + "/link/google/callback",
+		"authPath": "/link/google",
+		"callbackPath": "/link/google/callback",
+		"successRedirect": serverPath + "/auth/account",
+		"failureRedirect": "/login",
+		"scope": ["email", "profile"],
+		"link": true,
+		"session": false
+	},
 	"facebook-login": {
 		"provider": "facebook",
 		"module": "passport-facebook",
 		"clientID": process.env.FACEBOOK_AUTH_PROVIDER_ID,
 		"clientSecret": process.env.FACEBOOK_AUTH_PROVIDER_SECRET,
-		"callbackURL": "http://192.168.2.116:3000/auth/facebook/callback",
+		"callbackURL": serverPath + "/auth/facebook/callback",
 		"authPath": "/auth/facebook",
 		"callbackPath": "/auth/facebook/callback",
-		"successRedirect": "/api/Users/me",
+		"successRedirect": serverPath + "/api/Users/me",
 		"failureRedirect": "/login",
 		"scope": ["public_profile", "email"],
 		"profileFields": ["gender", "link", "locale", "name", "timezone",
@@ -56,18 +63,18 @@ module.exports = {
 		"session": false,
 		"json": true
 	},
-	// "facebook-link": {
-	// 	"provider": "facebook",
-	// 	"module": "passport-facebook",
-	// 	"clientID": process.env.FACEBOOK_AUTH_PROVIDER_ID,
-	// 	"clientSecret": process.env.FACEBOOK_AUTH_PROVIDER_SECRET,
-	// 	"callbackURL": "/link/facebook/callback",
-	// 	"authPath": "/link/facebook",
-	// 	"callbackPath": "/link/facebook/callback",
-	// 	"successRedirect": "/auth/account",
-	// 	"failureRedirect": "/login",
-	// 	"scope": ["email", "user_likes"],
-	// 	"link": true,
-	// 	"session": false
-	// }
+	"facebook-link": {
+		"provider": "facebook",
+		"module": "passport-facebook",
+		"clientID": process.env.FACEBOOK_AUTH_PROVIDER_ID,
+		"clientSecret": process.env.FACEBOOK_AUTH_PROVIDER_SECRET,
+		"callbackURL": serverPath + "/link/facebook/callback",
+		"authPath": "/link/facebook",
+		"callbackPath": "/link/facebook/callback",
+		"successRedirect": serverPath + "/auth/account",
+		"failureRedirect": "/login",
+		"scope": ["email", "user_likes"],
+		"link": true,
+		"session": false
+	}
 };
