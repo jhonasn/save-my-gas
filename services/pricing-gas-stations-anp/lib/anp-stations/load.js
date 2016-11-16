fa5f763dde868e9607cd1acb7e3a38df4b81998a
jhonasn <jhonasn@gmail.com>
2016-11-16 03:54:55 -0200

    implemented load to anp database, changed gas-station entity and deleted address and neighborhood entities by unecessary complexity

--- services/pricing-gas-stations-anp/lib/anp-stations/load.js
+++ services/pricing-gas-stations-anp/lib/anp-stations/load.js
@@ -1,7 +1,8 @@
 //this file will upload data to database
 var path = require('path')
+var async = require('async')
 var app = require('../../../../web-api/server/server')
-	// var entities = require(path.resolve(rootPath, 'lib', 'anp-stations', 'entities'))
+var entities = require(path.resolve(rootPath, 'lib', 'anp-stations', 'entities'))
 
 module.getCityState = function(cityId, states) {
 	var state = states.filter(function(st) {
@@ -17,272 +18,205 @@ module.getCityState = function(cityId, states) {
 	}
 }
 
-module.exports.load = function(entitiesPath, cb) {
-	var states = require(path.resolve(entitiesPath, 'states'))
-	var cities = require(path.resolve(entitiesPath, 'cities'))
-	var fuelTypes = require(path.resolve(entitiesPath, 'fuelTypes'))
-		// var stations = require(path.resolve(entitiesPath, 'stations'))
-		// var stationsProcessed = require(path.resolve(entitiesPath, 'ppStations'))
-	var stations = require(path.resolve(entitiesPath, 'ppStations'))
-
-
-	// ##########
-	// # mapping
-	// ##########
-	var statesWithCities = states
-	states = states.map(function(s) {
-		return {
-			name: s.name,
-			fu: s.id
-		}
-	})
-	console.log('mapeado estados')
-
-	cities = cities.map(function(c) {
-		var stateId = module.getCityState(c.id, statesWithCities)
-		stateId = stateId && stateId.id
-		return {
-			name: c.name,
-			anpId: c.id,
-			stateId: stateId //fu
-		}
-	})
-	console.log('mapeado cidades')
-
-	//get neighborhoods from gas stations
-	//remove duplicated
-	var neighborhoods = stations.reduce(function(prev, curr) {
-		if (!Array.isArray(prev)) {
-			if (prev.bairro.toLowerCase() === curr.bairro.toLowerCase()) {
-				return [prev]
-			} else {
-				return [prev, curr]
-			}
-		} else {
-			var existsNeighborhood = prev.some(function(p) {
-				return p.bairro.toLowerCase() === curr.bairro.toLowerCase()
-			})
-
-			if (!existsNeighborhood) {
-				prev.push(curr)
-			}
-			return prev
-		}
-	})
-
-	//pass stations with neighborhood info to mapping
-	neighborhoods.map(function(station) {
-		var cityState = module.getCityState(station.cityId, statesWithCities)
-		var stateId = cityState && cityState.id
-		return {
-			name: station.bairro,
-			cityId: station.cityId, //anpId
-			stateId: stateId //fu
-		}
-	})
-	console.log('mapeado bairros = ' + neighborhoods.length)
-
-	//get address from gas stations
-	//remove duplicated
-	var addresses = stations.reduce(function(prev, curr) {
-		if (!Array.isArray(prev)) {
-			if (prev.endereco.toLowerCase() === curr.endereco.toLowerCase()) {
-				return [prev]
-			} else {
-				return [prev, curr]
-			}
-		} else {
-			var existsAddress = prev.some(function(p) {
-				return p.endereco.toLowerCase() === curr.endereco.toLowerCase()
-			})
-
-			if (!existsAddress) {
-				prev.push(curr)
-			}
-			return prev
-		}
-	})
-
-	//pass stations with address info to mapping
-	addresses.map(function(station) {
-		var cityState = module.getCityState(station.cityId, statesWithCities)
-		var stateId = cityState && cityState.id
-		return {
-			address: station.endereco,
-			neighborhoodId: station.bairro,
-			stateId: stateId, //fu
-			cityId: station.cityId //anpId
-		}
-	})
-	console.log('mapeado endereços = ' + addresses.length)
-
-	fuelTypes.map(function(f) {
-		return {
-			name: f.name,
-			anpId: f.id
-		}
-	})
-	console.log('mapeado combustiveis = ' + fuelTypes.length)
-
-	//station
-	// "razaoSocial": String,
-	// "endereco": String,
-	// "bairro": String,
-	// "bandeira": String,
-	// "precoVenda": String,
-	// "precoCompra": String,
-	// "modalidadecompra": String,
-	// "fornecedorBBranca": String,
-	// "dataColeta": Date//json string
-	// "invoiceOk": Bool,
-	// "cityId": String,
-	// "fuelTypeId": Integer,
-	// "unit": String,
-
-	var fuelPrices = []
-	statition.forEach(function(s) {
-		fuelTypes.forEach(function(f) {
-			if (s[f.name]) {
-				fuelPrices.push(s[f.name])
-			}
-		})
-	})
-	console.log('mapeado preços de combustiveis = ', fuelPrices.length)
-
-	stations.map(function(s) {
-		var cityState = module.getCityState(s.cityId, statesWithCities)
-		var stateId = cityState && cityState.id
-		return {
-			companyName: s.razaoSocial,
-			geolocation: s.geolocation,
-			distributor: s.distribuidora,
-			invoiceOk: s.invoiceOk,
-			addressId: s.endereco,
-			neighborhoodId: s.bairro,
-			cityId: s.cityId,
-			stateId: stateId
-		}
-	})
-	console.log('mapeado postos = ', stations.length)
-
-	// entities.entities.sellPrices.map(function(s) {
-	// 	return {
-	// 		sale: {
-	// 	      type: number
-	// 	    },
-	// 	    purchase: {
-	// 	      type: number
-	// 	    },
-	// 	    supplierWhiteFlag: {
-	// 	      type: boolean
-	// 	    },
-	// 	    date: {
-	// 	      type: date
-	// 	    },
-	// 	    anpId: {
-	// 	      type: string
-	// 	    },
-	// 	    fuelTypeId: {
-	// 	      type: string
-	// 	    },
-	// 	    gasStationId: {
-	// 	      type: string
-	// 	    }
-	// 	}
-	// }).forEach(function(s) {
-	// 	app.models.fuelPrice.findOrCreate({
-	// 		name: s.name
-	// 	}, s)
-	// })
-
-	//######################
-	//# Load to database
-	//######################
+module.exports.load = function(entitiesPath, callback) {
 	app.datasources.mongodb.on('connected', function() {
-		states.forEach(function(s) {
-			app.models.state.findOrCreate({
-				fu: s.fu
-			}, s)
-		})
+		var states = require(path.resolve(entitiesPath, 'states'))
+		var cities = require(path.resolve(entitiesPath, 'cities'))
+		var fuelTypes = require(path.resolve(entitiesPath, 'fuelTypes'))
+		var stations = require(path.resolve(entitiesPath, 'stations'))
+		var stationsProcessed = require(path.resolve(entitiesPath, 'ppStations'))
 
-		cities.forEach(function(c) {
-			app.models.state.findOne({
-				where: {
-					fu: c.stateId
+		var putFuelTypes = function(fuelTypes) {
+			async.each(fuelTypes, function(f, cb) {
+				app.models.fuelType.findOrCreate({
+					anpId: f.id
+				}, {
+					name: f.name,
+					anpId: f.id
+				}, cb)
+			}, function(err) {
+				if (err) callback(err)
+
+				app.models.fuelType.find({}, function(err, dbFuelTypes) {
+					if (err) callback('error processing fuel types')
+
+					putStates(states, dbFuelTypes)
+				})
+			})
+		}
+
+		var putStates = function(states, dbFuelTypes) {
+			async.each(states, function(s, cb) {
+				app.models.state.findOrCreate({
+					fu: s.fu
+				}, {
+					name: s.name,
+					fu: s.id
+				}, cb)
+			}, function(err) {
+				if (err) throw err
+
+				app.models.state.find({}, function(err, dbStates) {
+					if (err) callback('error processing states')
+
+					putCities(cities, dbStates, dbFuelTypes)
+				})
+			})
+		}
+
+		var putCities = function(cities, dbStates, dbFuelTypes) {
+			async.each(cities, function(c, cb) {
+				var state = module.getCityState(c.id, states)
+				var dbState = dbStates.filter(function(ds) {
+					return ds.fu === state.id
+				})
+				if (!dbState.length) {
+					callback('it wasn\'t possible find the city\'s state ' + c.name)
 				}
-			}, function(err, state) {
-				c.stateId = !err ? state.id : c.stateId
+				dbState = dbState[0]
 				app.models.city.findOrCreate({
-					anpId: c.anpId
-				}, c)
+					anpId: c.id
+				}, {
+					name: c.name,
+					anpId: c.id,
+					stateId: dbState.id //fu
+				}, cb)
+			}, function(err) {
+				if(err) callback('error processing cities')
+
+				app.models.city.find({}, function(err, dbCities) {
+					if (err) callback(err)
+
+					putStations(stations, dbCities, dbFuelTypes)
+				})
 			})
-		})
+		}
 
-		neighborhoods.forEach(function(n) {
-			app.models.city.findOne({
-				where: {
-					anpId: n.cityId
-				},
-				includes: 'state'
-			}, function(err, city) {
-				if (!err && city.state) {
-					n.cityId = city.id
-					n.stateId = city.state.id
+		var putStations = function(stations, dbCities, dbFuelTypes) {
+			var i = 0
+			var stationsUnique = stations.filter(function(s) {
+				return s.invoiceOk
+			}).reduce(function(prev, curr) {
+				console.log('station reduce working: ', i++, '/', stations.length)
+				if (!Array.isArray(prev)) {
+					if (prev.razaoSocial.toLowerCase() === curr.razaoSocial.toLowerCase()) {
+						return [prev]
+					} else {
+						return [prev, curr]
+					}
+				} else {
+					var exists = prev.some(function(p) {
+						return p.razaoSocial.toLowerCase() === curr.razaoSocial.toLowerCase()
+					})
+
+					if (!exists) {
+						prev.push(curr)
+					}
+					return prev
 				}
-				app.models.neighborhood.findOrCreate({
-					name: n.name
-				}, n)
 			})
-		})
+			console.log('### station reduce completed ### ')
 
-		addresses.forEach(function(a) {
-			app.models.neighborhood.findOne({
-				where: {
-					name: a.neighborhoodId
-				},
-				includes: ['city', 'state']
-			}, function(err, neighborhood) {
-				if (!err && neighborhood.city && neighborhood.state) {
-					a.neighborhoodId = neighborhood.id
-					a.cityId = city.id
-					a.stateId = city.state.id
+			async.each(stationsUnique, function(s, cb) {
+				var dbCity = dbCities.filter(function(dc) {
+					return dc.anpId === s.cityId
+				})
+				if (!dbCity.length) {
+					callback('It wasn\'t possible to retrieve the city of gas station ' + s.razaoSocial)
 				}
-				app.models.address.findOrCreate({
-					address: a.address
-				}, a)
-			})
-		})
+				dbCity = dbCity[0]
 
-		fuelTypes.forEach(function(f) {
-			app.models.fuelType.findOrCreate({
-				anpId: f.anpId
-			}, f)
-		})
-
-		stations.forEach(function(s) {
-			app.models.address.findOne({
-				where: {
-					address: s.address
-				},
-				include: ['city', 'state', 'neighborhood']
-			}, function(err, address) {
-				if (
-					address &&
-					address.neighborhood &&
-					address.city &&
-					address.state
-				) {
-					s.addressId = address.id
-					s.neighborhoodId = address.neighborhood.id
-					s.cityId = address.city.id
-					s.stateId = address.state.id
+				//for first load to database
+				var stationProcessed = stationsProcessed.filter(function(sp) {
+					return sp.razaoSocial.toLowerCase() === s.razaoSocial.toLowerCase()
+				})
+				if (stationProcessed.length) {
+					stationProcessed = stationsProcessed[0]
 				}
+
 				app.models.gasStation.findOrCreate({
 					companyName: s.razaoSocial
-				}, s)
-			})
-		})
+				}, {
+					companyName: s.razaoSocial,
+					flag: notHiphen(s.bandeira),
+					geolocation: stationProcessed.geolocation,
+					distributor: notHiphen(s.distribuidora),
+					invoiceOk: s.invoiceOk,
+					address: s.endereco,
+					neighborhood: s.bairro,
+					cityId: dbCity.id,
+					stateId: dbCity.stateId
+				}, cb)
+			}, function(err) {
+				if (err) callback('error processing gas stations')
 
-		cb(null, 'ok!')
+				app.models.gasStation.find({ fields: { id: true, companyName: true } }, function(err, dbGasStations) {
+					putFuelPrices(stations, dbFuelTypes, dbGasStations)
+				})
+			})
+		}
+
+		var putFuelPrices = function(stations, dbFuelTypes, dbGasStations) {
+			stations = stations.filter(function(s) {
+				return s.invoiceOk
+			})
+
+			async.each(stations, function(s, cb) {
+				var dataColeta = brDateToDate(s.dataColeta)
+
+				console.log('fuel price ', stations.indexOf(s), '/', stations.length)
+
+				var dbFuelType = dbFuelTypes.filter(function(f) {
+					return f.anpId === s.fuelTypeId
+				})
+				if (!dbFuelType.length) {
+					callback('It wasn\'t possible to retrieve the fuel type of gas station ' + s.razaoSocial)
+				}
+				dbFuelType = dbFuelType[0]
+
+				var dbGasStation = dbGasStations.filter(function(gs) {
+					return gs.companyName.toLowerCase() === s.razaoSocial.toLowerCase()
+				})
+				if (!dbGasStation.length) {
+					callback('It wasn\'t possible to retrieve the gas station of fuel price ' + s.razaoSocial)
+				}
+				dbGasStation = dbGasStation[0]
+
+				app.models.fuelPrice.findOrCreate({
+					and: [{
+						date: dataColeta
+					}, {
+						fuelTypeId: dbFuelType.id
+					}]
+				}, {
+					sale: Number(notHiphen(s.precoVenda.replace(',', '.'))),
+					purchase: Number(notHiphen(s.precoCompra.replace(',', '.'))),
+					supplierWhiteFlag: notHiphen(s.fornecedorBBranca),
+					purchaseMode: notHiphen(s.modalidadecompra),
+					date: dataColeta,
+					unit: notHiphen(s.unit),
+					fuelTypeId: dbFuelType.id,
+					gasStationId: dbGasStation.id
+				}, cb)
+			}, function(err) {
+				if (err) callback('error processing fuel prices')
+
+				callback('load to database success')
+			})
+		}
+
+		var notHiphen = function (word) {
+			return word === '-' ? null : word
+		}
+
+		var brDateToDate = function(dt) {
+			dt = dt.split('/')
+			for (var i = 0; i < dt.length; i++) {
+				dt[i] = Number(dt[i])
+			}
+			return new Date(dt[2], (dt[1] - 1), dt[0])
+		}
+
+		putFuelTypes(fuelTypes)
 	})
 }
